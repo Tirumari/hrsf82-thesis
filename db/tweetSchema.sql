23e5aa48e16eb0736ca72cb5666151ee1ace9619
Enrique Hernandez <enrique.hernandez92@hotmail.com>
2017-11-06 16:16:19 -0800

    refactors tweet schema

--- db/tweetSchema.sql
+++ db/tweetSchema.sql
@@ -7,14 +7,13 @@ CREATE DATABASE tweeter;
 DROP TABLE IF EXISTS users;
 
 DROP TABLE IF EXISTS tweets;
-
+g
 CREATE TABLE tweets (
-  id serial PRIMARY KEY,
-  tweet_id varchar (100),
+  tweet_id varchar (100) PRIMARY KEY,
   user_id integer,
-  message varchar (255) NOT NULL,
+  message varchar (255),
   created_at varchar (100),
-  updated_at varchar (100),
+  updated_at decimal,
   impressions integer,
   views integer,
   likes integer,
